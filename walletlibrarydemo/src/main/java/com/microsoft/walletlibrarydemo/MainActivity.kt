d947406223ebd3397f216b3457812acdb21db8a2
Nithya Ganesh <niganesh@microsoft.com>
2022-12-14 22:58:44 -0500

    created a test function in library to return a string and displayed it in demo app

--- walletlibrarydemo/src/main/java/com/microsoft/walletlibrarydemo/MainActivity.kt
+++ walletlibrarydemo/src/main/java/com/microsoft/walletlibrarydemo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.microsoft.walletlibrarydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.microsoft.walletlibrary.VerifiedIdFlow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener { onClickButton() }
+
+    }
+
+    private fun onClickButton() {
+        val text = findViewById<TextView>(R.id.textview)
+        text.text = VerifiedIdFlow().init()
     }
 }
