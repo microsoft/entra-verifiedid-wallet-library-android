cc03ff64eeb6c7d77bd2477f1957ec2d45a5301d
Nithya Ganesh <niganesh@microsoft.com>
2022-12-30 17:10:04 -0500

    include vc sdk as a module

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/VerifiedIdFlow.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/VerifiedIdFlow.kt
@@ -1,10 +1,12 @@
 package com.microsoft.walletlibrary
 
 import android.content.Context
+import com.microsoft.did.sdk.VerifiableCredentialSdk
 
 class VerifiedIdFlow {
 
     fun init(context: Context): String {
-        return "Hello from library!!"
+        VerifiableCredentialSdk.init(context, "testingLibrary/1.0")
+        return VerifiableCredentialSdk.issuanceService.getNonce()
     }
 }
