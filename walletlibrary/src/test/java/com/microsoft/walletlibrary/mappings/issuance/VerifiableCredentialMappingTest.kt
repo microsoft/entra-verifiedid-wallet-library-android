46db25a8be6abe72ab6c2d63729c62dfb939f45a
Nithya Ganesh <niganesh@microsoft.com>
2023-03-01 14:52:24 -0500

    add unit tests for failing scenarios

--- walletlibrary/src/test/java/com/microsoft/walletlibrary/mappings/issuance/VerifiableCredentialMappingTest.kt
+++ walletlibrary/src/test/java/com/microsoft/walletlibrary/mappings/issuance/VerifiableCredentialMappingTest.kt
@@ -41,40 +41,16 @@ class VerifiableCredentialMappingTest {
 
     @Test
     fun mapVerifiableCredential_ProvideVerifiableCredential_ReturnsVerifiedId() {
+        // Act
         val actualResult = verifiableCredential.toVerifiedId()
+
+        // Assert
         assertThat(actualResult.id).isEqualTo(expectedJti)
         assertThat(actualResult.type).isEqualTo(VerifiedIdType.VERIFIABLE_CREDENTIAL)
         assertThat(actualResult.raw).isEqualTo(expectedRaw)
         assertThat(actualResult.claims.size).isEqualTo(1)
         assertThat(actualResult.claims.first().id).isEqualTo(expectedClaimName)
         assertThat(actualResult.claims.first().value).isEqualTo(expectedClaimValue)
-        assertThat(actualResult.issuedOn).isEqualTo(expectedIssuedOn)
-        assertThat(actualResult.expiresOn).isEqualTo(expectedExpiry)
-    }
-
-    @Test
-    fun mapVerifiableCredential_ProvideVerifiableCredential_ThrowsException() {
-
-        val expectedClaims = mutableMapOf<String, String>()
-        expectedClaims[expectedClaimName] = expectedClaimValue
-        verifiableCredentialDescriptor =
-            VerifiableCredentialDescriptor(emptyList(), emptyList(), emptyMap())
-        verifiableCredentialContent = VerifiableCredentialContent(
-            "testvccjti",
-            verifiableCredentialDescriptor,
-            "",
-            "",
-            expectedIssuedOn,
-            expectedExpiry
-        )
-        verifiableCredential =
-            VerifiableCredential(expectedJti, expectedRaw, verifiableCredentialContent)
-
-        val actualResult = verifiableCredential.toVerifiedId()
-        assertThat(actualResult.id).isEqualTo(expectedJti)
-        assertThat(actualResult.type).isEqualTo(VerifiedIdType.VERIFIABLE_CREDENTIAL)
-        assertThat(actualResult.raw).isEqualTo(expectedRaw)
-        assertThat(actualResult.claims.size).isEqualTo(0)
         assertThat(actualResult.issuedOn).isEqualTo(expectedIssuedOn)
         assertThat(actualResult.expiresOn).isEqualTo(expectedExpiry)
     }
