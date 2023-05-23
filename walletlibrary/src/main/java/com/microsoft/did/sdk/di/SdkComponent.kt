c3a3028450905f54700f674671fec086b81cd8c9
Nithya Ganesh <niganesh@microsoft.com>
2023-05-23 17:44:15 -0400

    reverted changes in import statements

--- walletlibrary/src/main/java/com/microsoft/did/sdk/di/SdkComponent.kt
+++ walletlibrary/src/main/java/com/microsoft/did/sdk/di/SdkComponent.kt
@@ -6,7 +6,13 @@
 package com.microsoft.did.sdk.di
 
 import android.content.Context
-import com.microsoft.did.sdk.*
+import com.microsoft.did.sdk.BackupService
+import com.microsoft.did.sdk.CorrelationVectorService
+import com.microsoft.did.sdk.IdentifierService
+import com.microsoft.did.sdk.IssuanceService
+import com.microsoft.did.sdk.LinkedDomainsService
+import com.microsoft.did.sdk.PresentationService
+import com.microsoft.did.sdk.RevocationService
 import dagger.BindsInstance
 import dagger.Component
 import kotlinx.serialization.modules.SerializersModule
