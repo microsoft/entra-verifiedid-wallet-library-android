bcb07c6c28d5e8aa9429d800c5f8e59c440e7826
Nithya Ganesh <niganesh@microsoft.com>
2023-07-27 16:58:55 -0400

    added issuer did to issuer style

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/mappings/issuance/IssuanceRequestMapping.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/mappings/issuance/IssuanceRequestMapping.kt
@@ -15,6 +15,7 @@ import com.microsoft.walletlibrary.requests.styles.VerifiedIdManifestIssuerStyle
 internal fun IssuanceRequest.getRequesterStyle(): RequesterStyle {
     return VerifiedIdManifestIssuerStyle(
         entityName,
+        entityIdentifier,
         contract.display.consent.title,
         contract.display.consent.instructions
     )
