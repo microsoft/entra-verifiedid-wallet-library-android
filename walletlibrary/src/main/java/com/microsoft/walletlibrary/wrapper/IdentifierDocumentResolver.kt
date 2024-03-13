539f766e1bdbb9a06546617790d91e2a131a2fef
Nithya Ganesh <niganesh@microsoft.com>
2024-03-13 18:14:21 -0400

    Added class comments for wrapper classes

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/wrapper/IdentifierDocumentResolver.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/wrapper/IdentifierDocumentResolver.kt
@@ -3,6 +3,9 @@ package com.microsoft.walletlibrary.wrapper
 import com.microsoft.walletlibrary.did.sdk.VerifiableCredentialSdk
 import com.microsoft.walletlibrary.did.sdk.identifier.models.identifierdocument.IdentifierDocument
 
+/**
+ * Wrapper class to wrap the resolve identifier from VC SDK and return identifier document.
+ */
 internal object IdentifierDocumentResolver {
 
     internal suspend fun resolveIdentifierDocument(did: String): Result<IdentifierDocument> {
--- walletlibrary/src/main/java/com/microsoft/walletlibrary/wrapper/RootOfTrustResolver.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/wrapper/RootOfTrustResolver.kt
@@ -6,6 +6,9 @@ import com.microsoft.walletlibrary.mappings.fetchAndVerifyLinkedDomains
 import com.microsoft.walletlibrary.mappings.toRootOfTrust
 import com.microsoft.walletlibrary.requests.RootOfTrust
 
+/**
+ * Wrapper class to wrap the fetch linked domains from VC SDK and return RootOfTrust.
+ */
 internal object RootOfTrustResolver {
 
     internal suspend fun resolveRootOfTrust(identifierDocument: IdentifierDocument): RootOfTrust {
