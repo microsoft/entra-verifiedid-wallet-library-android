18943b63c80d80e71a7bbe49a3663228c681ed0f
Nithya Ganesh <niganesh@microsoft.com>
2024-09-12 01:31:29 -0400

    added a log statement for decoding exception

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/VerifiedIdClient.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/VerifiedIdClient.kt
@@ -87,7 +87,7 @@ class VerifiedIdClient(
                 ).toVerifiedIdResult()
             }
         } catch (exception: SerializationException) {
-            SdkLog.i("Decoding the encoded string to verified ID failed and decoding it to Verifiable Credential: ${exception.javaClass.name} message ${exception.message}")
+            SdkLog.i("Decoding the encoded string to verified ID failed with exception ${exception.javaClass.name}, so attempting to decode it to Verifiable Credential.")
             val verifiableCredential = serializer.decodeFromString<com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential>(encodedVerifiedIdString)
             VerifiedIdResult.success(VerifiableCredential(toWalletLibraryVc(serializer, verifiableCredential)))
         }
