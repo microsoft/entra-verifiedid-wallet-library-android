1aeafc4383cb5ccb1bb62571d36eda575a64bcc8
Nithya Ganesh <niganesh@microsoft.com>
2024-09-12 01:41:15 -0400

    move VC conversion logic

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/VerifiedIdClient.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/VerifiedIdClient.kt
@@ -89,12 +89,9 @@ class VerifiedIdClient(
         } catch (exception: SerializationException) {
             SdkLog.i("Decoding the encoded string to verified ID failed with exception ${exception.javaClass.name}, so attempting to decode it to Verifiable Credential.")
             val verifiableCredential = serializer.decodeFromString<com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential>(encodedVerifiedIdString)
-            VerifiedIdResult.success(VerifiableCredential(toWalletLibraryVc(serializer, verifiableCredential)))
+            val vc = serializer.decodeFromString<com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential>(serializer.encodeToString(verifiableCredential))
+            VerifiedIdResult.success(VerifiableCredential(vc))
         }
-    }
-
-    private fun toWalletLibraryVc(jsonSerializer: Json, vc: com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential): com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential {
-        return jsonSerializer.decodeFromString(jsonSerializer.encodeToString(vc))
     }
 
     fun decodeVerifiedId(encodedVerifiedId: String): VerifiedIdResult<VerifiedId> {
