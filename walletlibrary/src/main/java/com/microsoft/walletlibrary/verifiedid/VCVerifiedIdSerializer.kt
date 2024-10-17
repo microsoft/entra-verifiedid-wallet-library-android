b3d57e89a74e16a22588bae8e3c444766c0a3845
nithyaganeshng <56405961+nithyaganeshng@users.noreply.github.com>
2024-10-17 13:45:44 -0400

    Make Verified ID implementation internal. (#87)
    Co-authored-by: Nithya Ganesh <niganesh@microsoft.com>

---
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/verifiedid/VCVerifiedIdSerializer.kt
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved
+
+package com.microsoft.walletlibrary.verifiedid
+
+import com.microsoft.walletlibrary.did.sdk.credential.service.models.contracts.VerifiableCredentialContract
+
+object VCVerifiedIdSerializer : VerifiedIdSerializer<com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential> {
+    /**
+     * Serialize the given verifiedID into the SerializedFormat
+     */
+    override fun serialize(verifiedId: VerifiedId): com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential {
+        return when (verifiedId) {
+            is VerifiableCredential -> {
+                verifiedId.raw
+            }
+
+            else -> {
+                throw VerifiedIdSerializer.VerifiedIdSerializationNotSupported()
+            }
+        }
+    }
+
+    fun deserialize(
+        rawVc: com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential,
+        contract: VerifiableCredentialContract? = null
+    ): VerifiedId {
+        return VerifiableCredential(rawVc, contract, rawVc.contents.vc.type)
+    }
+}
