beec445bff73ad03f55fb2e725f5fe537a91548a
Nithya Ganesh <niganesh@microsoft.com>
2024-10-21 18:48:33 -0400

    reuse the same string serializer

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/verifiedid/StringVerifiedIdSerializer.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/verifiedid/StringVerifiedIdSerializer.kt
@@ -1,13 +1,24 @@
 package com.microsoft.walletlibrary.verifiedid
 
+import kotlinx.serialization.json.Json
+
 object StringVerifiedIdSerializer : VerifiedIdSerializer<String> {
+
+    private val serializer = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    }
+
     /**
      * Serialize the given verifiedID into the SerializedFormat
      */
     override fun serialize(verifiedId: VerifiedId): String {
         when (verifiedId) {
             is VerifiableCredential -> {
-                return verifiedId.raw.raw
+                return serializer.encodeToString(
+                    com.microsoft.walletlibrary.did.sdk.credential.models.VerifiableCredential.serializer(),
+                    verifiedId.raw
+                )
             }
 
             else -> {
