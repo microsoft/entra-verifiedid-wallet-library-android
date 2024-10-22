f0949c5d4b3a2744f3effd7b9bafe4f9405e3d8b
Nithya Ganesh <niganesh@microsoft.com>
2024-10-22 17:42:10 -0400

    remove unused imports

--- walletlibrary/src/test/java/com/microsoft/walletlibrary/util/Serializer.kt
+++ walletlibrary/src/test/java/com/microsoft/walletlibrary/util/Serializer.kt
@@ -1,21 +1,6 @@
 package com.microsoft.walletlibrary.util
 
-import com.microsoft.walletlibrary.requests.ManifestIssuanceRequest
-import com.microsoft.walletlibrary.requests.OpenIdPresentationRequest
-import com.microsoft.walletlibrary.requests.VerifiedIdIssuanceRequest
-import com.microsoft.walletlibrary.requests.VerifiedIdPresentationRequest
-import com.microsoft.walletlibrary.requests.VerifiedIdRequest
-import com.microsoft.walletlibrary.requests.requirements.AccessTokenRequirement
-import com.microsoft.walletlibrary.requests.requirements.GroupRequirement
-import com.microsoft.walletlibrary.requests.requirements.IdTokenRequirement
-import com.microsoft.walletlibrary.requests.requirements.PinRequirement
-import com.microsoft.walletlibrary.requests.requirements.Requirement
-import com.microsoft.walletlibrary.requests.requirements.SelfAttestedClaimRequirement
-import com.microsoft.walletlibrary.requests.requirements.VerifiedIdRequirement
 import com.microsoft.walletlibrary.requests.styles.BasicVerifiedIdStyle
-import com.microsoft.walletlibrary.requests.styles.OpenIdVerifierStyle
-import com.microsoft.walletlibrary.requests.styles.RequesterStyle
-import com.microsoft.walletlibrary.requests.styles.VerifiedIdManifestIssuerStyle
 import com.microsoft.walletlibrary.requests.styles.VerifiedIdStyle
 import com.microsoft.walletlibrary.verifiedid.VerifiableCredential
 import com.microsoft.walletlibrary.verifiedid.VerifiedId
