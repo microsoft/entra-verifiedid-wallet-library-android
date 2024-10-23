b6d343279fe38abf1275285843f4260ad576d762
Nithya Ganesh <niganesh@microsoft.com>
2024-10-23 18:27:56 -0400

    Mapping to convert old identifier to new one and use it while signing the token.

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/did/sdk/credential/service/protectors/TokenSigner.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/did/sdk/credential/service/protectors/TokenSigner.kt
@@ -6,11 +6,8 @@
 package com.microsoft.walletlibrary.did.sdk.credential.service.protectors
 
 import com.microsoft.walletlibrary.did.sdk.crypto.keyStore.EncryptedKeyStore
-import com.microsoft.walletlibrary.did.sdk.crypto.protocols.jose.jws.JwsToken
 import com.microsoft.walletlibrary.did.sdk.identifier.models.Identifier
-import com.nimbusds.jose.JOSEObjectType
-import com.nimbusds.jose.JWSAlgorithm
-import com.nimbusds.jose.JWSHeader
+import com.microsoft.walletlibrary.mappings.identifier.toHolderIdentifier
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -19,14 +16,7 @@ internal class TokenSigner @Inject constructor(
     private val keyStore: EncryptedKeyStore
 ) {
     fun signWithIdentifier(payload: String, identifier: Identifier): String {
-        val token = JwsToken(payload, JWSAlgorithm.ES256K)
-        // adding kid value to header.
-        val header = JWSHeader.Builder(JWSAlgorithm.ES256K)
-            .type(JOSEObjectType.JWT)
-            .keyID("${identifier.id}#${identifier.signatureKeyReference}")
-            .build()
-        val privateKey = keyStore.getKey(identifier.signatureKeyReference)
-        token.sign(privateKey, header)
-        return token.serialize()
+        val holderIdentifier = identifier.toHolderIdentifier(keyStore)
+        return holderIdentifier.sign(payload.toByteArray()).decodeToString()
     }
 }
