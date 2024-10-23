3487c6cc33a5e3cef7103e3c195842194b329203
Nithya Ganesh <niganesh@microsoft.com>
2024-10-23 17:23:30 -0400

    Added a method to JWS Token to take an identifier and sign using it and unit test for it

--- walletlibrary/src/androidTest/java/com/microsoft/walletlibrary/did/sdk/crypto/protocols/jose/jws/JwsTokenTest.kt
+++ walletlibrary/src/androidTest/java/com/microsoft/walletlibrary/did/sdk/crypto/protocols/jose/jws/JwsTokenTest.kt
@@ -8,7 +8,7 @@ import com.microsoft.walletlibrary.did.sdk.crypto.KeyGenAlgorithm
 import com.microsoft.walletlibrary.did.sdk.crypto.keyStore.EncryptedKeyStore
 import com.microsoft.walletlibrary.did.sdk.crypto.keyStore.toPrivateJwk
 import com.microsoft.walletlibrary.identifier.EncryptedSharedPreferencesIdentifier
-import com.nimbusds.jose.JWSObject
+import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.KeyUse
 import org.assertj.core.api.Assertions
 import org.junit.Test
@@ -18,7 +18,7 @@ class JwsTokenTest {
     private val keyStore = EncryptedKeyStore(InstrumentationRegistry.getInstrumentation().targetContext)
 
     @Test
-    fun signToken_passHolderIdentifier_SignsToken() {
+    fun signToken_supplyHolderIdentifier_SignsToken() {
         // Arrange
         val privateKey = CryptoOperations.generateKeyPair(KeyGenAlgorithm.Secp256k1).toPrivateJwk("keyReferenceTest1", KeyUse.SIGNATURE)
         keyStore.storeKey("keyReferenceTest1", privateKey)
@@ -33,14 +33,15 @@ class JwsTokenTest {
         val testDataString = "{\"iss\":\"joe\",\n" +
             " \"exp\":1300819380,\n" +
             " \"http://example.com/is_root\":true}"
-        val jwsToken = JwsToken(JWSObject.parse(testDataString))
+        val jwsToken = JwsToken(testDataString.toByteArray(), JWSAlgorithm("ES256K"))
 
         // Act
-        jwsToken.sign(encryptedSharedPreferencesIdentifier)
+        val serializedToken = jwsToken.sign(encryptedSharedPreferencesIdentifier)
 
         // Assert
+        val deserializedToken = JwsToken.deserialize(serializedToken.decodeToString())
         val publicKey = keyStore.getKey("keyReferenceTest1").toPublicJWK()
         Assertions.assertThat(publicKey).isNotNull
-        Assertions.assertThat(jwsToken.verify(listOf(publicKey))).isTrue
+        Assertions.assertThat(deserializedToken.verify(listOf(publicKey))).isTrue
     }
 }
--- walletlibrary/src/main/java/com/microsoft/walletlibrary/did/sdk/crypto/protocols/jose/jws/JwsToken.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/did/sdk/crypto/protocols/jose/jws/JwsToken.kt
@@ -44,8 +44,8 @@ internal class JwsToken constructor(
         jwsObject.sign(signer)
     }
 
-    fun sign(identifier: HolderIdentifier) {
-        identifier.sign(jwsObject.payload.toBytes())
+    fun sign(identifier: HolderIdentifier): ByteArray {
+        return identifier.sign(jwsObject.payload.toBytes())
     }
 
     fun verify(publicKeyJWKs: List<JWK> = emptyList()): Boolean {
