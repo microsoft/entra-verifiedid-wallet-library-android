55871993408fb96efaa949c814c080a92f4f1f3f
Mau Esquivel Rogel <maesqui@microsoft.com>
2025-06-11 17:24:28 -0700

    Fix return type

--- walletlibrary/src/main/java/com/microsoft/walletlibrary/networking/entities/VerifiablePresentationResponse.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/networking/entities/VerifiablePresentationResponse.kt
@@ -3,11 +3,9 @@
 package com.microsoft.walletlibrary.networking.entities
 
 import com.microsoft.walletlibrary.verifiedid.PresentationVerified
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 internal data class VerifiablePresentationResponse (
-    @SerialName("redirect_uri")
     override val redirectUri: String? = null
 ) : PresentationVerified
--- walletlibrary/src/main/java/com/microsoft/walletlibrary/requests/handlers/OpenIdRequestProcessor.kt
+++ walletlibrary/src/main/java/com/microsoft/walletlibrary/requests/handlers/OpenIdRequestProcessor.kt
@@ -25,6 +25,7 @@ import com.microsoft.walletlibrary.util.NonceProcessor
 import com.microsoft.walletlibrary.util.PreviewFeatureFlags
 import com.microsoft.walletlibrary.util.RequirementCastingException
 import com.microsoft.walletlibrary.util.UnSupportedProtocolException
+import com.microsoft.walletlibrary.verifiedid.PresentationVerified
 import com.microsoft.walletlibrary.verifiedid.VerifiedId
 import com.microsoft.walletlibrary.wrapper.ManifestResolver
 
@@ -64,7 +65,7 @@ class OpenIdRequestProcessor internal constructor(private val libraryConfigurati
     private fun handlePresentationRequest(
         presentationRequestContent: PresentationRequestContent,
         rawRequest: OpenIdProcessedRequest
-    ): VerifiedIdRequest<Unit> {
+    ): VerifiedIdRequest<PresentationVerified> {
         var partialRequest = VerifiedIdPartialRequest(
             presentationRequestContent.requesterStyle,
             null,
